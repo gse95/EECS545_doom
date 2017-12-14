593a7cf03ec865fea4bfc2a21ff5a97e41483082
gse <gse@umich.edu>
2017-12-14 16:45:34 -0500

    drqn

--- examples/python/dqrn.py
+++ examples/python/dqrn.py
@@ -95,7 +95,7 @@ def create_network(session, available_actions_count):
                                             biases_initializer=tf.constant_initializer(0.1))
     conv2_flat = tf.contrib.layers.flatten(conv2)
 
-    conv2_flat = tf.reshape(conv2_flat,[batch_size,8, 384])
+    conv2_flat = tf.reshape(conv2_flat,[batch_size,8, 192])
     # fc1 = tf.contrib.layers.fully_connected(conv2_flat, num_outputs=128, activation_fn=tf.nn.relu,
     #                                         weights_initializer=tf.contrib.layers.xavier_initializer(),
     #                                         biases_initializer=tf.constant_initializer(0.1))
