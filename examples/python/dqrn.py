72fa3308967a74da2ac241d5fd2bdd47cd90734c
gse <gse@umich.edu>
2017-12-14 17:37:13 -0500

    drqn

--- examples/python/dqrn.py
+++ examples/python/dqrn.py
@@ -117,13 +117,13 @@ def create_network(session, available_actions_count):
                                           weights_initializer=tf.contrib.layers.xavier_initializer(),
                                           biases_initializer=tf.constant_initializer(0.1))
 
-    best_a = tf.argmax(q, 1)
-    print("best_A",best_a.shape)
+
 
     q = tf.reshape(q, [batch_size, 8, available_actions_count])
 
     q = tf.reduce_max(q,2)
-
+    best_a = tf.argmax(q, 1)
+    print("best_A", best_a.shape)
 
     loss = tf.losses.mean_squared_error(q, target_q_)
 
