91f3634f26fe953626b1cb1f2025d66df0eb9e06
gse <gse@umich.edu>
2017-12-14 20:29:03 -0500

    A3C

--- examples/python/VDoomA3C.py
+++ examples/python/VDoomA3C.py
@@ -266,7 +266,7 @@ tf.reset_default_graph()
 #if not os.path.exists('./frames'):
 #    os.makedirs('./frames')
 
-with tf.device("/cpu:0"): 
+with tf.device("/gpu:0"):
     global_episodes = tf.Variable(0,dtype=tf.int32,name='global_episodes',trainable=False)
     trainer = tf.train.AdamOptimizer(learning_rate=1e-4)
     master_network = AC_Network(s_size,a_size,'global',None) # Generate global network
