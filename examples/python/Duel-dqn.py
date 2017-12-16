de741951ed1ea7a401b2d05644b0f2c4ee4f8c40
gse <gse@umich.edu>
2017-12-16 15:49:27 -0500

    CSV

--- examples/python/Duel-dqn.py
+++ examples/python/Duel-dqn.py
@@ -27,7 +27,7 @@ test_episodes_per_epoch = 100
 
 # Other parameters
 frame_repeat = 12
-resolution = (30, 45)
+resolution = (50, 75)
 episodes_to_watch = 10
 
 model_savefile = "/tmp/model.ckpt"
@@ -85,11 +85,11 @@ def create_network(session, available_actions_count):
     target_q_ = tf.placeholder(tf.float32, [None, available_actions_count], name="TargetQ")
 
     # Add 2 convolutional layers with ReLu activation
-    conv1 = tf.contrib.layers.convolution2d(s1_, num_outputs=16, kernel_size=[6, 6], stride=[3, 3],
+    conv1 = tf.contrib.layers.convolution2d(s1_, num_outputs=32, kernel_size=[8, 8], stride=[4, 4],
                                             activation_fn=tf.nn.relu,
                                             weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                                             biases_initializer=tf.constant_initializer(0.1))
-    conv2 = tf.contrib.layers.convolution2d(conv1, num_outputs=32, kernel_size=[3, 3], stride=[2, 2],
+    conv2 = tf.contrib.layers.convolution2d(conv1, num_outputs=64, kernel_size=[4, 4], stride=[2, 2],
                                             activation_fn=tf.nn.relu,
                                             weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                                             biases_initializer=tf.constant_initializer(0.1))
@@ -100,8 +100,8 @@ def create_network(session, available_actions_count):
     Layer_V = tf.contrib.layers.flatten(Layer_VC)
 
     xav_i = tf.contrib.layers.xavier_initializer()
-    Weights_A = tf.Variable(xav_i([1280//2,available_actions_count]))
-    Weights_V = tf.Variable(xav_i([1280//2,1]))
+    Weights_A = tf.Variable(xav_i([4480//2,available_actions_count]))
+    Weights_V = tf.Variable(xav_i([4480//2,1]))
 
     Adv = tf.matmul(Layer_A,Weights_A)
     Val = tf.matmul(Layer_V,Weights_V)
